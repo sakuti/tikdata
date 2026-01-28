f77f957092ad05ac605c5a706cb4953f3fb3ab72
Saku <110587879+sakuti@users.noreply.github.com>
2026-01-28 21:51:18 +0200

    fix: turbopack workspace root path

--- interface/next.config.ts
+++ interface/next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["127.0.0.1"],
   output: "export",
   trailingSlash: true,
-  devIndicators: false
+  devIndicators: false,
+  turbopack: {
+    root: path.join(__dirname, '..'),
+  }
 };
 
 export default nextConfig;
